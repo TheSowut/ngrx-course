a97fa386d16344c12a5bb993e4837284d56e760b
TheSowut <sowutmail@gmail.com>
2022-07-31 14:41:31 +0300

    chore: auth reducer

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -1,18 +1,24 @@
 import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
+    createReducer, on
 } from '@ngrx/store';
+import { AuthActions } from '../action-types';
+import { User } from '../model/user.model';
 
 export const authFeatureKey = 'auth';
 
-export interface State {
-
-}
-
-export const reducers: ActionReducerMap<State> = {
-
+export interface AuthState {
+    user: User;
 };
 
+export const initialAuthState: AuthState = {
+    user: undefined
+}
+
+export const authReducer = createReducer(
+    initialAuthState,
+    on(AuthActions.login, (state, action) => {
+        return {
+            user: action.user
+        }
+    })
+);
